795d28a389e4c1c67f0392a550b6a9d5a0b9ed02
Felix Schmid <50250674+Felix-Schmid@users.noreply.github.com>
2023-12-15 23:09:17 +0100

    Add time table of taken slots

--- scripts.js
+++ scripts.js
@@ -1,4 +1,8 @@
 let jkudata = null
+const pauseStarts = new Set([600, 705, 810, 915, 1020, 1125, 1215, 1320])
+const startTime = 510
+const endTime = 1335
+const timeStep = 15
 
 /** fetch data from jkuroomsearch and create table */
 function init() {
@@ -7,11 +11,9 @@ function init() {
 		.then(data => { jkudata = data; createTable() })
 }
 
-/** create the main time table (unfilled) */
+/** create the main time table */
 function createTable() {
 	const body = document.body, tbl = document.createElement('table')
-	tbl.style.border = '1px solid black'
-	
 	const buildings = {}
 	
 	// sort rooms into buildings
@@ -20,9 +22,26 @@ function createTable() {
 		const bNo = r.building
 		
 		if (buildings[bNo] == undefined) {
-			buildings[bNo] = []
+			buildings[bNo] = {}
 		}
-		buildings[bNo].push(r)
+		buildings[bNo][room] = r
+	}
+	
+	// add header row
+	const tr = tbl.insertRow()
+	const td = tr.insertCell()
+	const str = document.createElement("strong")
+	str.appendChild(document.createTextNode("8"))
+	td.appendChild(str)
+	for (i = startTime; i < endTime; i += timeStep) {
+		const td = tr.insertCell()
+		if (i % 60 == 0) {
+			const str = document.createElement("strong")
+			str.appendChild(document.createTextNode(i / 60))
+			td.appendChild(str)
+		} else {
+			td.appendChild(document.createTextNode(i % 60))
+		}
 	}
 	
 	// add rows for each room
@@ -33,7 +52,6 @@ function createTable() {
 		const str = document.createElement("strong")
 		str.appendChild(document.createTextNode(jkudata.buildings[b].name))
 		td.appendChild(str)
-		td.style.border = '1px solid black'
 		
 		// add room rows for this building
 		for (r in buildings[b]) {
@@ -41,8 +59,31 @@ function createTable() {
 			const td = tr.insertCell()
 			const rm = buildings[b][r]
 			td.appendChild(document.createTextNode(`${rm.name} (${rm.capacity})`))
-			td.style.border = '1px solid black'
+			
+			for (i = startTime; i < endTime; i += timeStep) {
+				const td = tr.insertCell()
+				
+				if (!isAvailable(r, i, "2024-01-08")) { // TODO: hardcoded for now...
+					const div = document.createElement("div")
+					td.appendChild(div)
+					div.classList.add("unavailable")
+				}
+			}
 		}
 	}
 	body.appendChild(tbl)
+}
+
+function isAvailable(room, time, date) {
+	//if (pauseStarts.has(time)) {
+	//	return true;
+	//}
+	const day = jkudata.available[date]
+	
+	for (slot in day[room]) {
+		if (time >= day[room][slot][0] && time < day[room][slot][1]) {
+			return true;
+		}
+	}
+	return false;
 }
